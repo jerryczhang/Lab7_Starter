a7d99554917065f305042f0bf4f51b38b9bef684
Jerry Zhang <jerrychzhang@gmail.com>
2021-11-04 08:44:01 -0700

    Service Worker directory fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -50,7 +50,7 @@ async function init() {
 function initializeServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
         console.log('ServiceWorker registration success: ', registration.scope);
       }, function(err) {
         console.log('ServiceWorker registration failed: ', err)
