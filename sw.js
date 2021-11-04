c8b3c733348946de9b357f34f82ec11049e4bc38
Jerry Zhang <jerrychzhang@gmail.com>
2021-11-04 09:01:38 -0700

    Directory fix in caching URLs

--- sw.js
+++ sw.js
@@ -3,27 +3,26 @@
 
 const CACHE_NAME = 'lab-7-starter';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/favicon.ico',
-  '/assets',
-  '/assets/images',
-  '/assets/images/icons',
-  '/assets/images/icons/0-star.svg',
-  '/assets/images/icons/1-star.svg',
-  '/assets/images/icons/2-star.svg',
-  '/assets/images/icons/3-star.svg',
-  '/assets/images/icons/4-star.svg',
-  '/assets/images/icons/5-star.svg',
-  '/assets/images/icons/arrow-down.png',
-  '/assets/styles',
-  '/assets/styles/main.css',
-  '/assets/scripts',
-  '/assets/scripts/main.js',
-  '/assets/scripts/Router.js',
-  '/assets/components',
-  '/assets/components/RecipeCard.js',
-  '/assets/components/RecipeExpand.js'
+  'index.html',
+  'favicon.ico',
+  'assets',
+  'assets/images',
+  'assets/images/icons',
+  'assets/images/icons/0-star.svg',
+  'assets/images/icons/1-star.svg',
+  'assets/images/icons/2-star.svg',
+  'assets/images/icons/3-star.svg',
+  'assets/images/icons/4-star.svg',
+  'assets/images/icons/5-star.svg',
+  'assets/images/icons/arrow-down.png',
+  'assets/styles',
+  'assets/styles/main.css',
+  'assets/scripts',
+  'assets/scripts/main.js',
+  'assets/scripts/Router.js',
+  'assets/components',
+  'assets/components/RecipeCard.js',
+  'assets/components/RecipeExpand.js'
 ];
 
 // Once the service worker has been installed, feed it some initial URLs to cache
